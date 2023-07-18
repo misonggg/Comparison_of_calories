91f257112d862b384ee5fcec95362fbfebc6c789
misonggg <kimmisong1996@gmail.com>
2023-07-18 15:41:17 +0900

    first commit

---
+++ server/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('hello node');
+})
+
+app.listen(4000, () => console.log('4000번 포트에서 대기중'));
